547f7e0c2cc07d5e904f1db7d2198d64f66b3985
tarassh <tshchybo@gmail.com>
2024-08-16 23:57:08 -0700

    refactor

--- internal/ipfs-node/extract.go
+++ internal/ipfs-node/extract.go
@@ -3,7 +3,6 @@ package ipfsnode
 import (
 	"bytes"
 	"context"
-	"fmt"
 
 	"github.com/ipfs/go-cid"
 	"github.com/ipld/go-ipld-prime/datamodel"
@@ -21,121 +20,98 @@ func (ipfsNode *IPFSNode) ExtractBlock(ctx context.Context, cidStr string) (*ipl
 	}
 
 	// Retrieve the block from blockstore
-	node, err := ipfsNode.API.Dag().Get(context.Background(), cid)
-	if err != nil {
-		return nil, err
-	}
-
-	block, err := ipfsNode.decodeNode(ctx, cid, node.RawData())
+	node, err := ipfsNode.API.Dag().Get(ctx, cid)
 	if err != nil {
 		return nil, err
 	}
 
 	// Decode the block
-	return block, nil
+	return ipfsNode.decodeNode(ctx, cid, node.RawData())
 }
 
 func (ipfsNode *IPFSNode) decodeNode(ctx context.Context, rootCid cid.Cid, data []byte) (*ipldencoder.IPLDDataBlock, error) {
-	decoder, err := multicodec.LookupDecoder(uint64(rootCid.Type()))
+	root, err := ipfsNode.decodeData(rootCid, data)
 	if err != nil {
 		return nil, err
 	}
 
-	// Wrap the data in a bytes.Reader to satisfy io.Reader interface
-	reader := bytes.NewReader(data)
-
-	// Decode the data using the DAG-CBOR decoder
-	nb := basicnode.Prototype.Any.NewBuilder()
-
-	if err := decoder(nb, reader); err != nil {
-		return nil, fmt.Errorf("failed to decode CBOR data: %w", err)
-	}
-
-	root := nb.Build()
-
 	links, err := root.LookupByString("links")
 	if err != nil {
-		return nil, fmt.Errorf("failed to lookup links: %w", err)
+		return nil, err
 	}
 
-	var dataNodes [][]datamodel.Node
-	var linksNodes [][]datamodel.Link
-
-	iter := links.ListIterator()
-	for !iter.Done() {
-		_, linkNode, err := iter.Next()
-		if err != nil {
-			return nil, fmt.Errorf("failed to get link node: %w", err)
-		}
-
-		// Get the link CID
-
-		link, err := linkNode.AsLink()
-		if err != nil {
-			return nil, fmt.Errorf("failed to lookup link CID: %w", err)
-		}
-
-		linkCid, err := cid.Parse(link.String())
-		if err != nil {
-			return nil, err
-		}
-
-		// Get the link node
-		legacyNode, err := ipfsNode.API.Dag().Get(ctx, linkCid)
-		if err != nil {
-			return nil, fmt.Errorf("failed to get link node: %w", err)
-		}
-
-		links, datanodes, err := ipfsNode.decodeNode2(ctx, linkCid, legacyNode.RawData())
-		if err != nil {
-			return nil, err
-		}
-
-		linksNodes = append(linksNodes, links)
-		dataNodes = append(dataNodes, datanodes)
+	linkNodes, dataNodes, err := ipfsNode.processLinks(ctx, links)
+	if err != nil {
+		return nil, err
 	}
 
 	return &ipldencoder.IPLDDataBlock{
 		Version:   rootCid.Version(),
 		Codec:     rootCid.Type(),
-		Links:     linksNodes,
+		Links:     linkNodes,
 		DataNodes: dataNodes,
 		Root:      root,
 	}, nil
 }
 
-func (ipfsNode *IPFSNode) decodeNode2(ctx context.Context, rootCid cid.Cid, data []byte) ([]datamodel.Link, []datamodel.Node, error) {
-	decoder, err := multicodec.LookupDecoder(uint64(rootCid.Type()))
+func (ipfsNode *IPFSNode) processLinks(ctx context.Context, links datamodel.Node) ([][]datamodel.Link, [][]datamodel.Node, error) {
+	var linkNodes [][]datamodel.Link
+	var dataNodes [][]datamodel.Node
+
+	iter := links.ListIterator()
+	for !iter.Done() {
+		_, linkNode, err := iter.Next()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		link, err := linkNode.AsLink()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		linkCid, err := cid.Parse(link.String())
+		if err != nil {
+			return nil, nil, err
+		}
+
+		// Get the linked node
+		legacyNode, err := ipfsNode.API.Dag().Get(ctx, linkCid)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		links, data, err := ipfsNode.decodeLinkedNode(ctx, linkCid, legacyNode.RawData())
+		if err != nil {
+			return nil, nil, err
+		}
+
+		linkNodes = append(linkNodes, links)
+		dataNodes = append(dataNodes, data)
+	}
+
+	return linkNodes, dataNodes, nil
+}
+
+func (ipfsNode *IPFSNode) decodeLinkedNode(ctx context.Context, rootCid cid.Cid, data []byte) ([]datamodel.Link, []datamodel.Node, error) {
+	root, err := ipfsNode.decodeData(rootCid, data)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	// Wrap the data in a bytes.Reader to satisfy io.Reader interface
-	reader := bytes.NewReader(data)
-
-	// Decode the data using the DAG-CBOR decoder
-	nb := basicnode.Prototype.Any.NewBuilder()
-
-	if err := decoder(nb, reader); err != nil {
-		return nil, nil, fmt.Errorf("failed to decode CBOR data: %w", err)
-	}
-
-	root := nb.Build()
-
 	var dataNodes []datamodel.Node
 	var links []datamodel.Link
+
 	iter := root.ListIterator()
 	for !iter.Done() {
 		_, linkNode, err := iter.Next()
 		if err != nil {
-			return nil, nil, fmt.Errorf("failed to get link node: %w", err)
+			return nil, nil, err
 		}
-
-		// Get the link CID
 
 		link, err := linkNode.AsLink()
 		if err != nil {
-			return nil, nil, fmt.Errorf("failed to lookup link CID: %w", err)
+			return nil, nil, err
 		}
 
 		links = append(links, link)
@@ -145,13 +121,13 @@ func (ipfsNode *IPFSNode) decodeNode2(ctx context.Context, rootCid cid.Cid, data
 			return nil, nil, err
 		}
 
-		// Get the link node
+		// Get the linked node
 		legacyNode, err := ipfsNode.API.Dag().Get(ctx, linkCid)
 		if err != nil {
-			return nil, nil, fmt.Errorf("failed to get link node: %w", err)
+			return nil, nil, err
 		}
 
-		node, err := ipfsNode.decodeNode3(linkCid, legacyNode.RawData())
+		node, err := ipfsNode.decodeData(linkCid, legacyNode.RawData())
 		if err != nil {
 			return nil, nil, err
 		}
@@ -162,7 +138,7 @@ func (ipfsNode *IPFSNode) decodeNode2(ctx context.Context, rootCid cid.Cid, data
 	return links, dataNodes, nil
 }
 
-func (ipfsNode *IPFSNode) decodeNode3(rootCid cid.Cid, data []byte) (datamodel.Node, error) {
+func (ipfsNode *IPFSNode) decodeData(rootCid cid.Cid, data []byte) (datamodel.Node, error) {
 	decoder, err := multicodec.LookupDecoder(uint64(rootCid.Type()))
 	if err != nil {
 		return nil, err
@@ -171,11 +147,10 @@ func (ipfsNode *IPFSNode) decodeNode3(rootCid cid.Cid, data []byte) (datamodel.N
 	// Wrap the data in a bytes.Reader to satisfy io.Reader interface
 	reader := bytes.NewReader(data)
 
-	// Decode the data using the DAG-CBOR decoder
+	// Decode the data using the appropriate decoder
 	nb := basicnode.Prototype.Any.NewBuilder()
-
 	if err := decoder(nb, reader); err != nil {
-		return nil, fmt.Errorf("failed to decode CBOR data: %w", err)
+		return nil, err
 	}
 
 	return nb.Build(), nil
