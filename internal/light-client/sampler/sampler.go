f866e3320f96aa8a37a40463ef299b68f836a287
tarassh <tshchybo@gmail.com>
2024-08-17 22:01:57 -0700

    refactoring

--- internal/light-client/sampler/sampler.go
+++ internal/light-client/sampler/sampler.go
@@ -7,10 +7,9 @@ import (
 	"math/rand"
 	"strings"
 
-	logging "github.com/ipfs/go-log/v2"
-
 	"github.com/ipfs/go-cid"
 	ipfs "github.com/ipfs/go-ipfs-api"
+	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("light-client")
@@ -19,12 +18,12 @@ type Sampler struct {
 	IPFSShell *ipfs.Shell
 }
 
-// Struct for the Link
+// Link represents a link to another CID in IPFS.
 type Link struct {
 	CID string `json:"/"`
 }
 
-// Struct for the DAG node containing links
+// RootNode represents a DAG node containing metadata and links.
 type RootNode struct {
 	Version string `json:"version"`
 	Size    int    `json:"size"`
@@ -32,51 +31,48 @@ type RootNode struct {
 	Links   []Link `json:"links"`
 }
 
+// NestedBytes holds the base64 decoded bytes.
 type NestedBytes struct {
 	Bytes []byte `json:"bytes"`
 }
 
+// InnerMap represents a nested structure containing bytes data.
 type InnerMap struct {
 	Nested NestedBytes `json:"/"`
 }
 
+// DataMap represents a cell and its proof, used in the DAG structure.
 type DataMap struct {
 	Cell  InnerMap `json:"cell"`
 	Proof InnerMap `json:"proof"`
 }
 
-// UnmarshalJSON custom unmarshaler to handle base64 decoding directly into the Bytes field.
+// UnmarshalJSON handles base64 decoding directly into the Bytes field.
 func (n *NestedBytes) UnmarshalJSON(data []byte) error {
-	// Create an anonymous struct to unmarshal the raw string
 	var aux struct {
 		Bytes string `json:"bytes"`
 	}
 
-	// Unmarshal the JSON into the auxiliary struct
 	if err := json.Unmarshal(data, &aux); err != nil {
 		return fmt.Errorf("error unmarshaling bytes: %w", err)
 	}
 
-	bytesEncoded := addBase64Padding(aux.Bytes)
+	bytesWithPadding := ensureBase64Padding(aux.Bytes)
 
-	// Decode the base64 string
-	decoded, err := base64.StdEncoding.DecodeString(bytesEncoded)
+	decodedBytes, err := base64.StdEncoding.DecodeString(bytesWithPadding)
 	if err != nil {
 		return fmt.Errorf("error decoding base64 string: %w", err)
 	}
 
-	// Assign the decoded bytes to the Bytes field
-	n.Bytes = decoded
+	n.Bytes = decodedBytes
 	return nil
 }
 
-// NewSampler creates a new Sampler instance and checks if the IPFS daemon is running.
+// NewSampler creates a new Sampler instance and checks the connection to the IPFS daemon.
 func NewSampler(ipfsAddr string) (*Sampler, error) {
 	shell := ipfs.NewShell(ipfsAddr)
 
-	// Check if the IPFS daemon is running by getting the node's version
-	_, _, err := shell.Version()
-	if err != nil {
+	if _, _, err := shell.Version(); err != nil {
 		return nil, fmt.Errorf("failed to connect to IPFS daemon: %w", err)
 	}
 
@@ -85,61 +81,61 @@ func NewSampler(ipfsAddr string) (*Sampler, error) {
 	}, nil
 }
 
-// ProcessEvent handles the event received from the EventListener
+// ProcessEvent handles events asynchronously by processing the provided CID.
 func (s *Sampler) ProcessEvent(cidStr string) {
-	// Spawn a goroutine to handle the event processing asynchronously
 	go func(cidStr string) {
-		c, err := cid.Decode(cidStr)
+		cidDecoded, err := cid.Decode(cidStr)
 		if err != nil {
 			log.Errorf("Invalid CID: %v", err)
 			return
 		}
 
-		rootNode, err := s.fetchRootNode(c.String())
+		rootNode, err := s.getRootNode(cidDecoded.String())
 		if err != nil {
 			log.Errorf("Failed to fetch root DAG data: %v", err)
 			return
 		}
 
-		cols, err := s.fetchRandomRow(rootNode)
+		randomLinkIndex := getRandomIndex(len(rootNode.Links))
+		links, err := s.getLinks(rootNode.Links[randomLinkIndex].CID)
 		if err != nil {
-			log.Errorf("Failed to fetch data from random link: %v", err)
+			log.Errorf("Failed to fetch link data: %v", err)
 			return
 		}
 
-		var randomIndex int
-		_, err = s.fetchData(cols, &randomIndex)
+		randomLinkIndex = getRandomIndex(len(links))
+		_, err = s.getDataNode(links[randomLinkIndex].CID)
 		if err != nil {
-			log.Errorf("Failed to fetch data from random index: %v", err)
+			log.Errorf("Failed to fetch data node: %v", err)
 			return
 		}
 
-		// Implement the logic to send the proof and cell to the service
+		// Implement logic to send proof and cell to the service
 		// sendProofAndCell(proof, cell)
-	}(cidStr) // Pass cidStr to the anonymous function
+	}(cidStr)
 }
 
-func (s *Sampler) fetchRootNode(cidStr string) (*RootNode, error) {
-	var root RootNode
-	if err := s.IPFSShell.DagGet(cidStr, &root); err != nil {
+func (s *Sampler) getRootNode(cidStr string) (*RootNode, error) {
+	var rootNode RootNode
+	if err := s.IPFSShell.DagGet(cidStr, &rootNode); err != nil {
 		return nil, err
 	}
 
-	return &root, nil
+	return &rootNode, nil
 }
 
-// fetchDagData retrieves the DAG data for a given CID and unmarshals it into a slice of Link structs.
-func (s *Sampler) fetchCols(cidStr string) ([]Link, error) {
-	var dagData []Link
-	if err := s.IPFSShell.DagGet(cidStr, &dagData); err != nil {
+// getLinks retrieves the DAG data for a given CID and unmarshals it into a slice of Link structs.
+func (s *Sampler) getLinks(cidStr string) ([]Link, error) {
+	var links []Link
+	if err := s.IPFSShell.DagGet(cidStr, &links); err != nil {
 		return nil, err
 	}
 
-	return dagData, nil
+	return links, nil
 }
 
-// fetchDataNode retrieves the DAG data for a given CID
-func (s *Sampler) fetchDataNode(cidStr string) (*DataMap, error) {
+// getDataNode retrieves the DAG data for a given CID and unmarshals it into a DataMap struct.
+func (s *Sampler) getDataNode(cidStr string) (*DataMap, error) {
 	var data DataMap
 	if err := s.IPFSShell.DagGet(cidStr, &data); err != nil {
 		return nil, err
@@ -148,31 +144,16 @@ func (s *Sampler) fetchDataNode(cidStr string) (*DataMap, error) {
 	return &data, nil
 }
 
-// fetchDataFromRandomLink selects a random link and retrieves the DAG data
-func (s *Sampler) fetchRandomRow(root *RootNode) ([]Link, error) {
-	selectedLink := root.Links[rand.Intn(len(root.Links))]
-
-	return s.fetchCols(selectedLink.CID)
+// getRandomIndex returns a random index for a given length.
+func getRandomIndex(length int) int {
+	return rand.Intn(length)
 }
 
-// fetchData selects a random index and retrieves the DAG data
-func (s *Sampler) fetchData(links []Link, randomIndex *int) (*DataMap, error) {
-	// Fetch the data from the random index
-	*randomIndex = rand.Intn(int(len(links)))
-	data, err := s.fetchDataNode(links[*randomIndex].CID)
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
-}
-
-// addBase64Padding ensures the base64 string has correct padding
-func addBase64Padding(encoded string) string {
-	// Calculate the padding required
-	missingPadding := len(encoded) % 4
-	if missingPadding > 0 {
-		padding := 4 - missingPadding
+// ensureBase64Padding ensures the base64 string has correct padding.
+func ensureBase64Padding(encoded string) string {
+	paddingNeeded := len(encoded) % 4
+	if paddingNeeded > 0 {
+		padding := 4 - paddingNeeded
 		encoded += strings.Repeat("=", padding)
 	}
 	return encoded
