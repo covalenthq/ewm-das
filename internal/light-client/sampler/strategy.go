02f4fdb736d9467107dd87bbf91c99991e67c4b9
tarassh <tshchybo@gmail.com>
2024-09-12 09:23:02 -0700

    update sampling fn

--- internal/light-client/sampler/strategy.go
+++ internal/light-client/sampler/strategy.go
@@ -4,7 +4,11 @@ import "math"
 
 // CalculateSamplesNeeded calculates the minimum number of samples needed to achieve the desired confidence level.
 func CalculateSamplesNeeded(R, E int, C float64) int {
+	if R == 2 && E == 1 && C < 1 {
+		return 1
+	}
+
 	// Calculate the number of samples S using the given formula S \geq R - R \times \left(1 - C\right)^{\frac{1}{E}}
 	S := float64(R) - float64(R)*math.Pow(1-C, 1/float64(E))
-	return int(math.Floor(S)) // Round up to the nearest whole number since you can't take a fraction of a sample
+	return int(math.Ceil(S)) // Round up to the nearest whole number since you can't take a fraction of a sample
 }
