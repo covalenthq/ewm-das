d91e56d2c6e07909370a6517e6474a32a2e8e8e2
tarassh <tshchybo@gmail.com>
2024-09-11 19:19:21 -0700

    new sampling prototype

--- internal/light-client/c-kzg-verifier/verifier.go
+++ internal/light-client/c-kzg-verifier/verifier.go
@@ -8,7 +8,7 @@ import (
 
 // KZGVerifier represents a structure to verify KZG commitments.
 type KZGVerifier struct {
-	Commitment []byte
+	Commitment ckzg4844.Bytes48
 	Proof      []byte
 	Cell       []byte
 	Index      uint64
@@ -16,8 +16,11 @@ type KZGVerifier struct {
 
 // NewKZGVerifier creates a new instance of KZGVerifier with the given commitment, proof, and cell.
 func NewKZGVerifier(commitment, proof, cell []byte, index uint64) *KZGVerifier {
+	temp := ckzg4844.Bytes48{}
+	copy(temp[:], commitment)
+
 	return &KZGVerifier{
-		Commitment: commitment,
+		Commitment: temp,
 		Proof:      proof,
 		Cell:       cell,
 		Index:      index,
@@ -27,9 +30,8 @@ func NewKZGVerifier(commitment, proof, cell []byte, index uint64) *KZGVerifier {
 // Verify checks the validity of the proof against the commitment and cell.
 func (v *KZGVerifier) Verify() (bool, error) {
 	var (
-		commitment ckzg4844.Bytes48
-		proof      ckzg4844.Bytes48
-		cell       ckzg4844.Cell
+		proof ckzg4844.Bytes48
+		cell  ckzg4844.Cell
 	)
 
 	// verify the length of the commitment, proof, and cell
@@ -39,14 +41,54 @@ func (v *KZGVerifier) Verify() (bool, error) {
 		return false, fmt.Errorf("invalid length of commitment, proof, or cell")
 	}
 
-	copy(commitment[:], v.Commitment)
 	copy(proof[:], v.Proof)
 	copy(cell[:], v.Cell)
 
-	commitments := [1]ckzg4844.Bytes48{commitment}
+	commitments := [1]ckzg4844.Bytes48{v.Commitment}
 	proofs := [1]ckzg4844.Bytes48{proof}
 	cells := [1]ckzg4844.Cell{cell}
 	indexes := [1]uint64{v.Index}
 
 	return ckzg4844.VerifyCellKZGProofBatch(commitments[:], indexes[:], cells[:], proofs[:])
+}
+
+func (v *KZGVerifier) VerifyBatch() (bool, error) {
+	var (
+		proof   ckzg4844.Bytes48
+		cell    ckzg4844.Cell
+		indexes [1]uint64
+	)
+
+	if len(v.Commitment) != ckzg4844.BytesPerCommitment {
+		return false, fmt.Errorf("invalid length of commitment")
+	}
+
+	var results []bool
+	commitments := [1]ckzg4844.Bytes48{v.Commitment}
+	for i := 0; i < 64; i++ {
+
+		copy(proof[:], v.Proof[i*ckzg4844.BytesPerProof:(i+1)*ckzg4844.BytesPerProof])
+		copy(cell[:], v.Cell[i*ckzg4844.BytesPerCell:(i+1)*ckzg4844.BytesPerCell])
+
+		indexes[0] = v.Index*64 + uint64(i)
+		proofs := [1]ckzg4844.Bytes48{proof}
+		cells := [1]ckzg4844.Cell{cell}
+
+		res, err := ckzg4844.VerifyCellKZGProofBatch(commitments[:], indexes[:], cells[:], proofs[:])
+		if err != nil {
+			return false, err
+		}
+		results = append(results, res)
+	}
+
+	return all(results), nil
+}
+
+func all(results []bool) bool {
+	for _, res := range results {
+		if !res {
+			return false
+		}
+	}
+	return true
 }
--- internal/light-client/sampler/strategy.go
+++ internal/light-client/sampler/strategy.go
@@ -6,5 +6,5 @@ import "math"
 func CalculateSamplesNeeded(R, E int, C float64) int {
 	// Calculate the number of samples S using the given formula S \geq R - R \times \left(1 - C\right)^{\frac{1}{E}}
 	S := float64(R) - float64(R)*math.Pow(1-C, 1/float64(E))
-	return int(math.Ceil(S)) // Round up to the nearest whole number since you can't take a fraction of a sample
+	return int(math.Floor(S)) // Round up to the nearest whole number since you can't take a fraction of a sample
 }
