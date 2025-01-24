5fbb57e9354f07f1b7f680c842c7376e43cf65bb
tarassh <tshchybo@gmail.com>
2025-01-24 10:56:16 -0800

    use 5080 port

--- scripts/das-pinner-tester.sh
+++ scripts/das-pinner-tester.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 echo Waiting for pinner to start...
-while ! nc -z ewm-das 3001; do
+while ! nc -z ewm-das 5080; do
   sleep 1
 done
 
 echo -e "\nPinner is deployed"
 
 echo -e "\nTesting endpoint /api/v1/cid..."
-wget --quiet --method=POST --body-file=/root/.covalent/test/specimen.bin --header="Content-Type: multipart/form-data" -O - http://ewm-das:3001/api/v1/cid
+wget --quiet --method=POST --body-file=/root/.covalent/test/specimen-result.json --header="Content-Type: multipart/form-data" -O - http://ewm-das:5080/api/v1/cid
 
 echo -e "\nTesting endpoint /api/v1/upload..."
-wget --quiet --method=POST --body-file=/root/.covalent/test/specimen.bin --header="Content-Type: multipart/form-data" -O - http://ewm-das:3001/api/v1/upload
+wget --quiet --method=POST --body-file=/root/.covalent/test/specimen-result.json --header="Content-Type: multipart/form-data" -O - http://ewm-das:5080/api/v1/upload
 
 echo -e "\nTesting endpoint /api/v1/download..."
-wget --quiet -O - http://ewm-das:3001/api/v1/download?cid=bafyreiaboifhabwe5opprpc2sfq2eab7n4csfksx2mywtg6sok4qcswblu
+wget --quiet -O - http://ewm-das:5080/api/v1/download?cid=bafyreibo6rb2gvqi5srunoypym3tfzlbbj2yohmcbhpnrb47zexsugfeim
 
 echo -e "\nTesting endpoint /upload..."
-wget --quiet --method=POST --body-file=/root/.covalent/test/specimen.bin --header="Content-Type: multipart/form-data" -O - http://ewm-das:3001/upload
+wget --quiet --method=POST --body-file=/root/.covalent/test/specimen-result.json --header="Content-Type: multipart/form-data" -O - http://ewm-das:5080/upload
 
 echo -e "\nTesting endpoint /get..."
-wget --quiet -O - http://ewm-das:3001/get?cid=bafyreiaboifhabwe5opprpc2sfq2eab7n4csfksx2mywtg6sok4qcswblu
+wget --quiet -O - http://ewm-das:5080/get?cid=bafyreibo6rb2gvqi5srunoypym3tfzlbbj2yohmcbhpnrb47zexsugfeim
 
 echo -e "\nTesting endpoint /cid..."
-wget --quiet --method=POST --body-file=/root/.covalent/test/specimen.bin --header="Content-Type: multipart/form-data" -O - http://ewm-das:3001/cid
+wget --quiet --method=POST --body-file=/root/.covalent/test/specimen-result.json --header="Content-Type: multipart/form-data" -O - http://ewm-das:5080/cid
 
 exit 0
