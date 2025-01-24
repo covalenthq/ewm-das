8b8d34c86a6a14ce8bb14de01f9b1943f8665ce5
tarassh <tshchybo@gmail.com>
2025-01-24 09:28:56 -0800

    [docker compose]use wget for the tester part

--- scripts/das-pinner-tester.sh
+++ scripts/das-pinner-tester.sh
@@ -7,21 +7,21 @@ done
 echo -e "\nPinner is deployed"
 
 echo -e "\nTesting endpoint /api/v1/cid..."
-curl -F "filedata=@/root/.covalent/test/specimen.bin" http://ewm-das:3001/api/v1/cid
+wget --quiet --method=POST --body-file=/root/.covalent/test/specimen.bin --header="Content-Type: multipart/form-data" -O - http://ewm-das:3001/api/v1/cid
 
 echo -e "\nTesting endpoint /api/v1/upload..."
-curl -F "filedata=@/root/.covalent/test/specimen.bin" http://ewm-das:3001/api/v1/upload
+wget --quiet --method=POST --body-file=/root/.covalent/test/specimen.bin --header="Content-Type: multipart/form-data" -O - http://ewm-das:3001/api/v1/upload
 
 echo -e "\nTesting endpoint /api/v1/download..."
-curl http://ewm-das:3001/api/v1/download?cid=bafyreiaboifhabwe5opprpc2sfq2eab7n4csfksx2mywtg6sok4qcswblu
+wget --quiet -O - http://ewm-das:3001/api/v1/download?cid=bafyreiaboifhabwe5opprpc2sfq2eab7n4csfksx2mywtg6sok4qcswblu
 
 echo -e "\nTesting endpoint /upload..."
-curl -F "filedata=@/root/.covalent/test/specimen.bin" http://ewm-das:3001/upload
+wget --quiet --method=POST --body-file=/root/.covalent/test/specimen.bin --header="Content-Type: multipart/form-data" -O - http://ewm-das:3001/upload
 
 echo -e "\nTesting endpoint /get..."
-curl http://ewm-das:3001/get?cid=bafyreiaboifhabwe5opprpc2sfq2eab7n4csfksx2mywtg6sok4qcswblu
+wget --quiet -O - http://ewm-das:3001/get?cid=bafyreiaboifhabwe5opprpc2sfq2eab7n4csfksx2mywtg6sok4qcswblu
 
 echo -e "\nTesting endpoint /cid..."
-curl -F "filedata=@/root/.covalent/test/specimen.bin" http://ewm-das:3001/cid
+wget --quiet --method=POST --body-file=/root/.covalent/test/specimen.bin --header="Content-Type: multipart/form-data" -O - http://ewm-das:3001/cid
 
 exit 0
