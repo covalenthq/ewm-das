03749c7f97242695990f136ed789da8effe769dc
Pranay Valson <pranay.valson@gmail.com>
2025-01-23 20:52:33 -0800

    rewrite proper workflow paths, check cid first
    Signed-off-by: Pranay Valson <pranay.valson@gmail.com>

--- scripts/das-pinner-tester.sh
+++ scripts/das-pinner-tester.sh
@@ -5,11 +5,12 @@ while ! nc -z ewm-das 3001; do
 done
 
 echo -e "\nPinner is deployed"
-echo -e "\nTesting endpoint /api/v1/upload..."
-curl -F "filedata=@/root/.covalent/test/specimen.bin" http://ewm-das:3001/api/v1/upload
 
 echo -e "\nTesting endpoint /api/v1/cid..."
 curl -F "filedata=@/root/.covalent/test/specimen.bin" http://ewm-das:3001/api/v1/cid
+
+echo -e "\nTesting endpoint /api/v1/upload..."
+curl -F "filedata=@/root/.covalent/test/specimen.bin" http://ewm-das:3001/api/v1/upload
 
 echo -e "\nTesting endpoint /api/v1/download..."
 curl http://ewm-das:3001/api/v1/download?cid=bafyreiaboifhabwe5opprpc2sfq2eab7n4csfksx2mywtg6sok4qcswblu
@@ -17,8 +18,10 @@ curl http://ewm-das:3001/api/v1/download?cid=bafyreiaboifhabwe5opprpc2sfq2eab7n4
 echo -e "\nTesting endpoint /upload..."
 curl -F "filedata=@/root/.covalent/test/specimen.bin" http://ewm-das:3001/upload
 
+echo -e "\nTesting endpoint /get..."
+curl http://ewm-das:3001/get?cid=bafyreiaboifhabwe5opprpc2sfq2eab7n4csfksx2mywtg6sok4qcswblu
+
 echo -e "\nTesting endpoint /cid..."
 curl -F "filedata=@/root/.covalent/test/specimen.bin" http://ewm-das:3001/cid
 
-echo -e "\nTesting endpoint /get..."
-curl http://ewm-das:3001/get?cid=bafyreiaboifhabwe5opprpc2sfq2eab7n4csfksx2mywtg6sok4qcswblu
+exit 0
